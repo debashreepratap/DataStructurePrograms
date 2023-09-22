eaa2c97d0da09243a7bb7e884987970759722007
Debashree.pratap <debashree.pratap@happiestminds.com>
2023-09-22 14:42:28 +0530

    first commit

---
+++ src/main/java/com/interview/Eployeee.java
@@ -0,0 +1,15 @@
+package com.interview;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class Eployeee {
+
+    private String name;
+    private int salary;
+    private String department;
+}
